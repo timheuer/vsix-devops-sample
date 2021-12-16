d939c55e9c234ddd7c8dcae17aa4f42a441b37ee
Tim Heuer <tim@timheuer.com>
2021-12-16 13:29:01 -0800

    Add project files.

---
+++ Commands/MyCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Community.VisualStudio.Toolkit;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace InsertGuid
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            DocumentView docView = await VS.Documents.GetActiveDocumentViewAsync();
+            var position = docView.TextView?.Selection.Start.Position.Position;
+
+            if (position.HasValue)
+            {
+                docView.TextBuffer.Insert(position.Value, Guid.NewGuid().ToString());
+            }
+        }
+    }
+}
